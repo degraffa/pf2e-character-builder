062e1b2ff40b54c8b9b79a1c3168a7f329c93b54
degraffa <ard96@cornell.edu>
2021-09-05 22:06:49 -0400

    cleaned up packages

--- src/components/hero-sheet/HeroSheetSidebar.tsx
+++ src/components/hero-sheet/HeroSheetSidebar.tsx
@@ -1,56 +1,12 @@
 import React from 'react';
-import Sidebar from 'react-sidebar';
 
 interface Props {}
-interface State {isDocked: boolean, isOpen: boolean}
-
-const mql = window.matchMedia('(mid-width: 800px');
+interface State {}
 
 export default class HeroSheetSidebar extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      isDocked: mql.matches,
-      isOpen: false
-    }
-
-    this.onSetOpen = this.onSetOpen.bind(this);
-    this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
-  }
-
-  componentWillMount(): void {
-    mql.addListener(this.mediaQueryChanged);
-  }
-
-  componentWillUnmount(): void {
-    mql.removeListener(this.mediaQueryChanged);
-  }
-
-  onSetOpen(isOpen: boolean): void {
-    this.setState({isOpen: isOpen});
-  }
-
-  mediaQueryChanged() {
-    this.setState({isDocked: mql.matches, isOpen: false});
-  }
-
   render() {
     return (
-      <Sidebar
-        sidebar={<b>Sidebar content</b>}
-        open={this.state.isOpen}
-        docked={this.state.isDocked}
-        onSetOpen={this.onSetOpen}
-        styles={{sidebar: {background: "white"}}}
-      >
-
-        <div className="sidebar-button"> 
-          <button onClick={() => this.onSetOpen(true)}>
-            <img src={`${process.env.PUBLIC_URL}/assets/menu-bars.png`} alt="Open Sidebar" className="sidebar-image"/>
-          </button>
-        </div>
-      </Sidebar>
-    )
+      <div></div>
+    );
   }
 }
